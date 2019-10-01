9762d9af39902e3fc6e1761725095c6439c62ea1
sigtot <sigurdtotland@gmail.com>
2019-10-01 08:52:38 +0200

    Implement stdin solution

--- acronyms.py
+++ acronyms.py
@@ -1,3 +1,22 @@
 #!/usr/bin/env python3
 
-print("Hello world")
+import fileinput
+
+words = {}
+
+
+def sorted_string(s: str) -> str:
+    return "".join(sorted(s))
+
+
+for line in fileinput.input():
+    word = line.rstrip()
+    word_key = sorted_string(word)
+    if words.get(word_key) is None:
+        words[word_key] = [word]
+    else:
+        words[word_key].append(word)
+
+for acronyms in words.values():
+    if len(acronyms) > 1:
+        print(", ".join(acronyms))

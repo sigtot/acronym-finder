a217b5cb832469e778a41ecef7632689de2146cc
sigtot <sigurdtotland@gmail.com>
2019-10-01 09:39:35 +0200

    Handle when input file does not exist

--- acronyms.py
+++ acronyms.py
@@ -9,13 +9,17 @@ def sorted_string(s: str) -> str:
     return "".join(sorted(s))
 
 
-for line in fileinput.input(mode="r"):
-    word = line.rstrip()
-    word_key = sorted_string(word)
-    if words.get(word_key) is None:
-        words[word_key] = [word]
-    else:
-        words[word_key].append(word)
+try:
+    for line in fileinput.input(mode="r"):
+        word = line.rstrip()
+        word_key = sorted_string(word)
+        if words.get(word_key) is None:
+            words[word_key] = [word]
+        else:
+            words[word_key].append(word)
+except FileNotFoundError:
+    exit("No such file")
+
 
 for acronyms in words.values():
     if len(acronyms) > 1:
